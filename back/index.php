2b644e61b6103c661200239d1f27f29796e1fe18
mackliu <macklun@gmail.com>
2022-01-04 11:19:49 +0800

    作品集作業規範

--- back/index.php
+++ back/index.php
@@ -5,9 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="shortcut icon" href="#" type="image/x-icon">
-    <title>履歷表後台管理</title>
+    <title>作品集後台管理</title>
 </head>
 <body>
-<h1>履歷表管理後台</h1>
+<h1>作品集管理後台</h1>
 </body>
 </html>
--- index.php
+++ index.php
@@ -5,9 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="shortcut icon" href="#" type="image/x-icon">
-    <title>我的線上履歷表</title>
+    <title>作品集</title>
 </head>
 <body>
-    <h1>線上履歷表</h1>
+    <h1>作品集</h1>
 </body>
 </html>
